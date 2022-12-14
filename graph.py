d959d09c3fa5f7615c1d5226310e56664dafc760
DeniseIra02 <irajubilo@gmail.com>
2022-12-14 20:42:44 +0800

    this def function takes another node as an argument and optionally order the neighbors using a custom strategy. It also defines an empty dictionary that populate when visiting a neighbor by associating it with the previous node.

--- graph.py
+++ graph.py
@@ -52,6 +52,23 @@ def breadth_first_search(graph, source, predicate):
     for node in breadth_first_traverse(graph, source):
         if predicate(node):
             return node
+
+def shortest_path(graph, source, destination, order_by=None):
+    queue = Queue(source)
+    visited = {source}
+    previous = {}
+    while queue:
+        node = queue.dequeue()
+        neighbors = list(graph.neighbors(node))
+        if order_by:
+            neighbors.sort(key=order_by)
+        for neighbor in neighbors:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.enqueue(neighbor)
+                previous[neighbor] = node
+                if neighbor == destination:
+                    return retrace(previous, source, destination)
         
 #Test:
 
@@ -141,10 +158,10 @@ def breadth_first_search(graph, source, predicate):
 #     print(city.name)
 
 #Shortest Path Using Breadth-First Traversal
-nodes, graph = load_graph("roadmap.dot", City.from_dict)
+# nodes, graph = load_graph("roadmap.dot", City.from_dict)
 
-city1 = nodes["aberdeen"]
-city2 = nodes["perth"]
+# city1 = nodes["aberdeen"]
+# city2 = nodes["perth"]
 
-for i, path in enumerate(nx.all_shortest_paths(graph, city1, city2), 1):
-    print(f"{i}.", " → ".join(city.name for city in path))
+# for i, path in enumerate(nx.all_shortest_paths(graph, city1, city2), 1):
+#     print(f"{i}.", " → ".join(city.name for city in path))
