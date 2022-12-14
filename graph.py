d5b6edc8bfb74a99a66b441c26c1f812cdd45c79
DeniseIra02 <irajubilo@gmail.com>
2022-12-14 20:58:26 +0800

    Natural Order of Neighbors test.

--- graph.py
+++ graph.py
@@ -179,3 +179,20 @@ def retrace(previous, source, destination):
 
 # for i, path in enumerate(nx.all_shortest_paths(graph, city1, city2), 1):
 #     print(f"{i}.", " → ".join(city.name for city in path))
+
+#Natural Order of Neighbors
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
+
+city1 = nodes["aberdeen"]
+city2 = nodes["perth"]
+
+print(" → ".join(city.name for city in shortest_path(graph, city1, city2)))
+
+def by_latitude(city):
+    return -city.latitude
+
+print(" → ".join(
+    city.name
+    for city in shortest_path(graph, city1, city2, by_latitude)
+))
+
