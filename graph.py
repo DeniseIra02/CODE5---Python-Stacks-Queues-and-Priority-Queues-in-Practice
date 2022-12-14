6c98a4260b7790ded65356335d6c0a7148aea67f
DeniseIra02 <irajubilo@gmail.com>
2022-12-14 16:12:50 +0800

    inserted City class with name tuple and extend it to ensure that node objects are hashable. Note: name tuple is anextension and custom data type that enrich built-in tuples with extra utilities.

--- graph.py
+++ graph.py
@@ -5,10 +5,27 @@ from typing import NamedTuple
 import os
 os.add_dll_directory("C:/Program Files/Graphviz/bin")
 
+class City(NamedTuple):
+    name: str
+    country: str
+    year: int | None
+    latitude: float
+    longitude: float
+    
+    @classmethod
+    def from_dict(cls, attrs):
+        return cls(
+            name = attrs["xlabel"],
+            country = attrs["country"]
+            year = int(attrs["year"]) or None,
+            latitute = float(attrs["latitude"]),
+            longitude = float(attrs["longitude"]),
+        )
+    
 #Test:
 #use pygraphviz to read the sample DOT file
 #print(nx.nx_agraph.read_dot("roadmap.dot"))
 
 #reading DOT file (nodes)
-graph = nx.nx_agraph.read_dot("roadmap.dot")
-print(graph.nodes["london"])
+# graph = nx.nx_agraph.read_dot("roadmap.dot")
+# print(graph.nodes["london"])
