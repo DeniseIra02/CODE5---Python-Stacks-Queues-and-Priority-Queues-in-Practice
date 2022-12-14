49dafb88bec26e58e298d5ca3ff96465591f1554
DeniseIra02 <irajubilo@gmail.com>
2022-12-14 15:57:35 +0800

    implemented NamedTuple from the module typing

--- graph.py
+++ graph.py
@@ -1,4 +1,6 @@
 import networkx as nx
+from typing import NamedTuple
+
 #directory path for graphviz
 import os
 os.add_dll_directory("C:/Program Files/Graphviz/bin")
@@ -7,6 +9,6 @@ os.add_dll_directory("C:/Program Files/Graphviz/bin")
 #use pygraphviz to read the sample DOT file
 #print(nx.nx_agraph.read_dot("roadmap.dot"))
 
-#reading DOT file
+#reading DOT file (nodes)
 graph = nx.nx_agraph.read_dot("roadmap.dot")
 print(graph.nodes["london"])
