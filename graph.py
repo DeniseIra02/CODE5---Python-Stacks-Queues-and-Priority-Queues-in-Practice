b659c4628c2ba4e69523a974a39ff265c737e844
DeniseIra02 <irajubilo@gmail.com>
2022-12-14 15:56:15 +0800

    add test to read DOT file(node)

--- graph.py
+++ graph.py
@@ -3,4 +3,10 @@ import networkx as nx
 import os
 os.add_dll_directory("C:/Program Files/Graphviz/bin")
 
+#Test:
+#use pygraphviz to read the sample DOT file
 #print(nx.nx_agraph.read_dot("roadmap.dot"))
+
+#reading DOT file
+graph = nx.nx_agraph.read_dot("roadmap.dot")
+print(graph.nodes["london"])
