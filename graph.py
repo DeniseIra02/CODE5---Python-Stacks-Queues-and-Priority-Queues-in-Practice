9d2267d200a64055cf5dc88b5ddb835dc8320aff
DeniseIra02 <irajubilo@gmail.com>
2022-12-14 21:09:14 +0800

    two nodes connected or not test

--- graph.py
+++ graph.py
@@ -184,19 +184,25 @@ def connected(graph, source, destination):
 # for i, path in enumerate(nx.all_shortest_paths(graph, city1, city2), 1):
 #     print(f"{i}.", " → ".join(city.name for city in path))
 
-#Natural Order of Neighbors
+# #Natural Order of Neighbors
+# nodes, graph = load_graph("roadmap.dot", City.from_dict)
+
+# city1 = nodes["aberdeen"]
+# city2 = nodes["perth"]
+
+# print(" → ".join(city.name for city in shortest_path(graph, city1, city2)))
+
+# def by_latitude(city):
+#     return -city.latitude
+
+# print(" → ".join(
+#     city.name
+#     for city in shortest_path(graph, city1, city2, by_latitude)
+# ))
+
+#Connected or not
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
 
-city1 = nodes["aberdeen"]
-city2 = nodes["perth"]
-
-print(" → ".join(city.name for city in shortest_path(graph, city1, city2)))
-
-def by_latitude(city):
-    return -city.latitude
-
-print(" → ".join(
-    city.name
-    for city in shortest_path(graph, city1, city2, by_latitude)
-))
+print(connected(graph, nodes["belfast"], nodes["glasgow"]))
+print(connected(graph, nodes["belfast"], nodes["derry"]))
 
