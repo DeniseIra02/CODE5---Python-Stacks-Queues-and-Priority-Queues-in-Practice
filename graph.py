18e6965f86680f0d109f22c24fdc6692fcadb76e
DeniseIra02 <irajubilo@gmail.com>
2022-12-14 22:09:31 +0800

    this part is the implementation of Dijkstra’s algorithm. setting unvisited to mutableminheap

--- graph.py
+++ graph.py
@@ -34,22 +34,25 @@ def load_graph(filename, node_factory):
         name: node_factory(attributes)
         for name, attributes in graph.nodes(data=True)
     }
-    
     return nodes, nx.Graph(
         (nodes[name1], nodes[name2], weights)
         for name1, name2, weights in graph.edges(data=True)
     )
-    
-def breadth_first_traverse(graph, source):
+
+
+def breadth_first_traverse(graph, source, order_by=None):
     queue = Queue(source)
     visited = {source}
     while queue:
         yield (node := queue.dequeue())
-        for neighbor in graph.neighbors(node):
+        neighbors = list(graph.neighbors(node))
+        if order_by:
+            neighbors.sort(key=order_by)
+        for neighbor in neighbors:
             if neighbor not in visited:
                 visited.add(neighbor)
                 queue.enqueue(neighbor)
-                
+
 def breadth_first_search(graph, source, predicate, order_by=None):
     return search(breadth_first_traverse, graph, source, predicate, order_by)
 
@@ -121,6 +124,28 @@ def search(traverse, graph, source, predicate, order_by=None):
     for node in traverse(graph, source, order_by):
         if predicate(node):
             return node
+
+def dijkstra_shortest_path(graph, source, destination, weight_factory):
+    previous = {}
+    visited = set()
+
+    unvisited = MutableMinHeap()
+    for node in graph.nodes:
+        unvisited[node] = infinity
+    unvisited[source] = 0
+
+    while unvisited:
+        visited.add(node := unvisited.dequeue())
+        for neighbor, weights in graph[node].items():
+            if neighbor not in visited:
+                weight = weight_factory(weights)
+                new_distance = unvisited[node] + weight
+                if new_distance < unvisited[neighbor]:
+                    unvisited[neighbor] = new_distance
+                    previous[neighbor] = node
+
+    return retrace(previous, source, destination)
+
 
 #Test:
 
