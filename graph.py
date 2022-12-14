9665648277937c81ccff2947b235f3512a015177
DeniseIra02 <irajubilo@gmail.com>
2022-12-14 16:59:14 +0800

    Test to sort the weights of the neighors by define function that returns a list of neighbors and their weights sorted by the specified strategy and define function that produces a floating-point distance based on the input dictionary.

--- graph.py
+++ graph.py
@@ -34,7 +34,7 @@ def load_graph(filename, node_factory):
         (nodes[name1], nodes[name2], weights)
         for name1, name2, weights in graph.edges(data=True)
     )
-    
+        
 #Test:
 
 #use pygraphviz to read the sample DOT file
@@ -55,6 +55,17 @@ def load_graph(filename, node_factory):
 #     print(neighbor.name)
 
 #to identify the neighbors with weights of the connecting edges
+# nodes, graph = load_graph("roadmap.dot", City.from_dict)
+# for neighbor, weights in graph[nodes["london"]].items():
+#     print(weights["distance"], neighbor.name)
+
+#sorting the weights of the neighbors in accending order
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
-for neighbor, weights in graph[nodes["london"]].items():
-    print(weights["distance"], neighbor.name)
+def sort_by(neighbors, strategy):
+    return sorted(neighbors.items(), key=lambda item: strategy(item[1]))
+
+def by_distance(weights):
+    return float(weights["distance"])
+
+for neighbor, weights in sort_by(graph[nodes["london"]], by_distance):
+    print(f"{weights['distance']:>3} miles, {neighbor.name}")
