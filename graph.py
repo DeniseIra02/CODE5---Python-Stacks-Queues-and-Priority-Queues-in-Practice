e6c6712c2261c6fc9cc55e716c483eca87688f08
DeniseIra02 <irajubilo@gmail.com>
2022-12-14 15:48:49 +0800

    Adding directory path for graphviz.

--- graph.py
+++ graph.py
@@ -1,1 +1,6 @@
-import networkx as nx 
+import networkx as nx
+#directory path for graphviz
+import os
+os.add_dll_directory("C:/Program Files/Graphviz/bin")
+
+#print(nx.nx_agraph.read_dot("roadmap.dot"))
