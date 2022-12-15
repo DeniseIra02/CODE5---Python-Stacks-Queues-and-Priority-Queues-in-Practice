5cee3f700283f08f04cc7d1bfb93faff7efa2c32
DeniseIra02 <irajubilo@gmail.com>
2022-12-16 01:38:17 +0800

    add necessary rich.module and import rich classes from it.

--- thread_safe_queues.py
+++ thread_safe_queues.py
@@ -6,6 +6,12 @@ from queue import LifoQueue, PriorityQueue, Queue
 from random import randint
 from time import sleep
 
+from rich.align import Align
+from rich.columns import Columns
+from rich.console import Group
+from rich.live import Live
+from rich.panel import Panel
+
 QUEUE_TYPES = {
     "fifo": Queue,
     "lifo": LifoQueue,
